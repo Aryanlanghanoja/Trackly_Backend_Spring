f944fd06e38de8c55849f34f1c3c01e919f50728
Aryanlanghanoja <aryanlanghanoja233@gmail.com>
2025-05-17 21:41:43 +0530

    added project

---
+++ src/main/java/com/LMS/Trackly/TracklyBackendSpringApplication.java
@@ -0,0 +1,13 @@
+package com.LMS.Trackly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TracklyBackendSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TracklyBackendSpringApplication.class, args);
+	}
+
+}
